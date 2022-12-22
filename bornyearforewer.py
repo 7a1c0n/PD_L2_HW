b4b7b4f39ffbd9976eaf3686ae0bef27b1d95d2e
scholar <sokolov.feliks@gmail.com>
2022-12-22 22:44:26 +0100

    Initial commit

---
+++ bornyearforewer.py
@@ -0,0 +1,7 @@
+while 'user isn''t right':
+    year = int(input('В каком году родился А.С.Пушкин?: '))
+    if year == 1799:
+        print('Верно')
+        break
+    else:
+        print('Неверно')
---
+++ variables.py
@@ -0,0 +1,12 @@
+# характеристики бильярдных игр
+
+name = 'Snooker'
+table_length_ft = 11.667
+number_of_balls = 22
+is_balls_colored = True
+
+print(type(name))
+print(type(table_length_ft))
+print(type(number_of_balls))
+print(type(is_balls_colored))
+
